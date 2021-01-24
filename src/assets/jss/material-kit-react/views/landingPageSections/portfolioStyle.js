bf8390916324029ff7de7d2e3c92f7e1d8ff2c51
JakobWestergarden <jakobwestergarden@gmail.com>
2021-01-24 19:16:48 +0100

    Added portfoliopage

---
+++ src/assets/jss/material-kit-react/views/landingPageSections/portfolioStyle.js
@@ -0,0 +1,95 @@
+/*import { cardTitle, title } from "assets/jss/material-kit-react.js";
+import imagesStyle from "assets/jss/material-kit-react/imagesStyles.js";
+
+const teamStyle = {
+  section: {
+    padding: "70px 0",
+    textAlign: "center"
+  },
+  title: {
+    ...title,
+    textAlign: "center",
+    marginBottom: "1rem",
+    marginTop: "30px",
+    minHeight: "32px",
+    textDecoration: "none",
+  },
+
+  subtitle: {
+    textAlign: "center",
+    marginBottom: "1rem",
+    minHeight: "32px",
+    textDecoration: "none",
+    color: "#999",
+  },
+  ...imagesStyle,
+  itemGrid: {
+    marginLeft: "auto",
+    marginRight: "auto"
+  },
+  description: {
+    color: "#999"
+  },
+  justifyCenter: {
+    justifyContent: "center !important"
+  },
+};
+
+export default teamStyle;*/
+
+import { cardTitle, title } from "assets/jss/material-kit-react.js";
+import imagesStyle from "assets/jss/material-kit-react/imagesStyles.js";
+
+const teamStyle = {
+  section: {
+    padding: "70px 0",
+    textAlign: "center"
+  },
+  title: {
+    ...title,
+    textAlign: "center",
+    marginBottom: "1rem",
+    marginTop: "30px",
+    minHeight: "32px",
+    textDecoration: "none",
+  },
+
+  subtitle: {
+    textAlign: "center",
+    marginBottom: "1rem",
+    minHeight: "32px",
+    textDecoration: "none",
+    color: "#999",
+  },
+  ...imagesStyle,
+  itemGrid: {
+    marginLeft: "auto",
+    marginRight: "auto"
+  },
+  cardTitle,
+  smallTitle: {
+    color: "#6c757d"
+  },
+  description: {
+    color: "#999"
+  },
+  justifyCenter: {
+    justifyContent: "center !important"
+  },
+  socials: {
+    marginTop: "0",
+    width: "200%",
+    transform: "none",
+    left: "0",
+    top: "0",
+    height: "200%",
+    lineHeight: "41px",
+    fontSize: "20px",
+    color: "#999"
+  },
+  margin5: {
+    margin: "5px"
+  }
+};
+
+export default teamStyle;
--- src/views/PortfolioPage/Sections/PortfolioSection.js
+++ src/views/PortfolioPage/Sections/PortfolioSection.js
@@ -1,4 +1,5 @@
 import React from "react";
+// nodejs library that concatenates classes
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -7,24 +8,159 @@ import { makeStyles } from "@material-ui/core/styles";
 // core components
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
+import Card from "components/Card/Card.js";
+import CardBody from "components/Card/CardBody.js";
+import CardFooter from "components/Card/CardFooter.js";
 
+import styles from "assets/jss/material-kit-react/views/landingPageSections/portfolioStyle.js";
 
-import styles from "assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
+import eztv2 from "assets/img/eztv2.png";
+import watersystem from "assets/img/watersystem.png";
+import trading from "assets/img/trading.jpg";
+
 
 const useStyles = makeStyles(styles);
 
-export default function ProductSection() {
+export default function PortfolioSection() {
   const classes = useStyles();
   return (
     <div className={classes.section}>
       <GridContainer justify="center">
-        <GridItem xs={12} sm={12} md={8}>
-          <h2 className={classes.title}>Produktportfölj</h2>
-          <h5 className={classes.description}>
-            Här Github, nån embedded modul kanske.
-          </h5>
+        <GridItem xs={12} sm={12} md={12}>
+          <h2 className={classes.title}>Produktportfölj:</h2>  
+          <h5 className={classes.subtitle}>Nedan syns produkter skapade av utvecklarna på ATcron:</h5>
         </GridItem>
       </GridContainer>
+      <div>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={6}>
+            <Card plain>
+              <img src={eztv2} alt="..."  />
+              <h4 className={classes.title}>
+                EasyTV
+                <br />
+                <small className={classes.smallTitle}>För enklare tv.</small>
+              </h4>
+              <CardBody>
+                <p className={classes.description}>
+                EasyTV är ett projekt skapat av Victor Gunnarsson tillsammans med Pontus Sahlenbo. Tanken bakom EasyTV är att skapa en intuitivare variant av svtplay för att lättare kunna hitta populära program och kunna bläddra i alfabetisk ordning.
+                </p>
+              </CardBody>
+              <CardFooter className={classes.justifyCenter}>
+                <Button
+                  justIcon
+                  color="transparent"
+                  className={classes.margin5}
+                >
+                  <i className={classes.socials + " fab fa-github"} />
+                </Button>
+                <Button
+                  justIcon
+                  color="transparent"
+                  className={classes.margin5}
+                >
+                  <i className={classes.socials + " fab fa-youtube"} />
+                </Button>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={6}>
+            <Card plain>
+              <img src={watersystem} alt="..."  />
+              <h4 className={classes.title}>
+                EasyTV
+                <br />
+                <small className={classes.smallTitle}>För enklare TV.</small>
+              </h4>
+              <CardBody>
+                <p className={classes.description}>
+                EasyTV är ett projekt skapat av Victor Gunnarsson tillsammans med Pontus Sahlenbo. Tanken bakom EasyTV är att skapa en intuitivare variant av svtplay för att lättare kunna hitta populära program och kunna bläddra i alfabetisk ordning.
+                </p>
+              </CardBody>
+              <CardFooter className={classes.justifyCenter}>
+                <Button
+                  justIcon
+                  color="transparent"
+                  className={classes.margin5}
+                >
+                  <i className={classes.socials + " fab fa-github fa-2x"} />
+                </Button>
+                <Button
+                  justIcon
+                  color="transparent"
+                  className={classes.margin5}
+                >
+                  <i className={classes.socials + " fab fa-youtube"} />
+                </Button>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={6}>
+            <Card plain>
+              <img src={eztv2} alt="..."  />
+              <h4 className={classes.title}>
+                EasyTV
+                <br />
+                <small className={classes.smallTitle}>För enklare tv.</small>
+              </h4>
+              <CardBody>
+                <p className={classes.description}>
+                EasyTV är ett projekt skapat av Victor Gunnarsson tillsammans med Pontus Sahlenbo. Tanken bakom EasyTV är att skapa en intuitivare variant av svtplay för att lättare kunna hitta populära program och kunna bläddra i alfabetisk ordning.
+                </p>
+              </CardBody>
+              <CardFooter className={classes.justifyCenter}>
+                <Button
+                  justIcon
+                  color="transparent"
+                  className={classes.margin5}
+                >
+                  <i className={classes.socials + " fab fa-github"} />
+                </Button>
+                <Button
+                  justIcon
+                  color="transparent"
+                  className={classes.margin5}
+                >
+                  <i className={classes.socials + " fab fa-youtube"} />
+                </Button>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={6}>
+            <Card plain>
+              <img src={eztv2} alt="..."  />
+              <h4 className={classes.title}>
+                EasyTV
+                <br />
+                <small className={classes.smallTitle}>För enklare tv.</small>
+              </h4>
+              <CardBody>
+                <p className={classes.description}>
+                EasyTV är ett projekt skapat av Victor Gunnarsson tillsammans med Pontus Sahlenbo. Tanken bakom EasyTV är att skapa en intuitivare variant av svtplay för att lättare kunna hitta populära program och kunna bläddra i alfabetisk ordning.
+                </p>
+              </CardBody>
+              <CardFooter className={classes.justifyCenter}>
+                <Button
+                  justIcon
+                  color="transparent"
+                  className={classes.margin5}
+                >
+                  <i className={classes.socials + " fab fa-github"} />
+                </Button>
+                <Button
+                  justIcon
+                  color="transparent"
+                  className={classes.margin5}
+                >
+                  <i className={classes.socials + " fab fa-youtube"} />
+                </Button>
+              </CardFooter>
+            </Card>
+          </GridItem>
+        </GridContainer>
+      </div>
     </div>
   );
 }
+
